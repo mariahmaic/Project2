0aeaf3df94dc72332576f4ebad2e08ac99d95c7f
Mariah <mariahmaic23@gmail.com>
2022-10-18 00:02:14 -0500

    Added about controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
 
   get "home", to: "home#index"
+  get "about", to: "about#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
