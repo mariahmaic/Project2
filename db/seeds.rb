7788c8fd01b71544f3c3b672ba2210b2a94d7716
Mariah <mariahmaic23@gmail.com>
2022-10-17 23:42:17 -0500

    Added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+200.times do
+  character = Faker::JapaneseMedia::StudioGhibli.character
+  studioghiblis = StudioGhibli.new(
+    character: character,
+    quote: Faker::JapaneseMedia::StudioGhibli.quote,
+    movie: Faker::JapaneseMedia::StudioGhibli.movie
+  )
+  studioghiblis.save
+end
